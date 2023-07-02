a4e0c5cf890cdb96b07cc7e4e017ee0d9701549b
Anton Mosich <anton@mosich.at>
2023-07-02 14:33:45 +0200

    qgis: 3.28.3 -> 3.28.8

--- pkgs/applications/gis/qgis/unwrapped.nix
+++ pkgs/applications/gis/qgis/unwrapped.nix
@@ -73,14 +73,14 @@ let
     six
   ];
 in mkDerivation rec {
-  version = "3.28.3";
+  version = "3.28.8";
   pname = "qgis-unwrapped";
 
   src = fetchFromGitHub {
     owner = "qgis";
     repo = "QGIS";
     rev = "final-${lib.replaceStrings [ "." ] [ "_" ] version}";
-    hash = "sha256-nXauZSC78BX1fcx0SXniwQpRmdSLfoqZ5jlbXeHgRGI=";
+    hash = "sha256-JYxWz9XjivjtktJ+Wwdj/+YvyfbYloseDUAabZXgBrk=";
   };
 
   passthru = {
