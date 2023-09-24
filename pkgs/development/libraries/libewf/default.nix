bdb06ccdb0db874a5691ae3a2af5a8419d0c44ef
Anton Mosich <anton@mosich.at>
2023-09-24 19:02:27 +0200

    libewf: 20201230 -> 20230212

--- pkgs/development/libraries/libewf/default.nix
+++ pkgs/development/libraries/libewf/default.nix
@@ -1,21 +1,13 @@
 { fetchurl, fetchpatch, lib, stdenv, zlib, openssl, libuuid, pkg-config, bzip2 }:
 
 stdenv.mkDerivation rec {
-  version = "20201230";
+  version = "20230212";
   pname = "libewf";
 
   src = fetchurl {
     url = "https://github.com/libyal/libewf/releases/download/${version}/libewf-experimental-${version}.tar.gz";
-    hash = "sha256-10r4jPzsA30nHQzjdg/VkwTG1PwOskwv8Bra34ZPMgc=";
+    hash = "sha256-0i7svZYsPX1kbM+6Ex/DwH5qB9o33BY7bsuxNI2xYQE=";
   };
-
-  patches = [
-    # fix build with OpenSSL 3.0
-    (fetchpatch {
-      url = "https://github.com/libyal/libewf/commit/033ea5b4e5f8f1248f74a2ec61fc1be183c6c46b.patch";
-      hash = "sha256-R4+NO/91kiZP48SJyVF9oYjKCg1h/9Kh8/0VOEmJXPQ=";
-    })
-  ];
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ zlib openssl libuuid ]
